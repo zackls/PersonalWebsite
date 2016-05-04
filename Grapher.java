ce368278eadf824d7f7788f521041498cc88d53a
Zack Littke-Smith <zdizzler@yahoo.com>
2016-05-04 01:20:51 -0400

    got the emulator working, among other things

--- Grapher.java
+++ Grapher.java
@@ -1,70 +1,279 @@
 import java.applet.Applet;
+
+import java.security.AccessController;
 import java.security.PrivilegedAction;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.awt.TextField;
+
+import java.awt.event.ActionListener;
+
 import javax.script.Bindings;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.text.MaskFormatter;
 
 public class Grapher extends Applet
 {
+    //width/height of the image
+    public float width = 450;
+    public float height = 450;
     //The following four numbers are the range you wish to view on
-    static private float minX;
-    static private float maxX;
-    static private float minY;
-    static private float maxY;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
     //Higher accuracy means longer but more accurate picture generating
-    static private float accuracy;
+    public float accuracy = 1;
     //Higher tolerance means less precise drawing
-    static private float tolerance = (float)1.0 / 600;
-    static private String l;
-    static private String r;
-    static private ScriptEngineManager manager = new ScriptEngineManager();
-    static private ScriptEngine engine = manager.getEngineByName("js");
-    public void graph(String l, String r, float minX, float maxX, float minY, float maxY, float accuracy)
+    public float tolerance = (float)1.0 / 300;
+    public String l;
+    public String r;
+
+    public ScriptEngineManager manager = new ScriptEngineManager();
+    public ScriptEngine engine = manager.getEngineByName("js");
+
+    public JButton graphButton = new JButton("Graph!");
+    public TextField leftText = new TextField("-X * Math.sin(X)", 25);
+    public TextField rightText = new TextField("Y * Math.sin(Y)", 25);
+    public TextField maxXText = new TextField("10.0", 7);
+    public TextField minXText = new TextField("-10.0", 7);
+    public TextField maxYText = new TextField("10.0", 7);
+    public TextField minYText = new TextField("-10.0", 7);
+    public JLabel leftLabel = new JLabel("f(x, y)");
+    public JLabel rightLabel = new JLabel("g(x, y)");
+    public JLabel bottomEqualsLabel = new JLabel("=");
+    public JLabel topEqualsLabel = new JLabel("=");
+    public JLabel maxXLabel = new JLabel("Max x:");
+    public JLabel minXLabel = new JLabel("Min x:");
+    public JLabel maxYLabel = new JLabel("Max y:");
+    public JLabel minYLabel = new JLabel("Min y:");
+    public JLabel graphingText = new JLabel("Graphing");
+
+    public JLabel graphedImage = new JLabel(new ImageIcon(new BufferedImage((int)width, (int)height, BufferedImage.TYPE_INT_ARGB)));
+
+    public String error = "";
+
+    public static Grapher instance = new Grapher();
+    public static JFrame frame = new JFrame();
+
+    public static void main(String[] args)
     {
-        this.l = l;
-        this.r = r;
-        this.minX = minX;
-        this.maxX = maxX;
-        this.minY = minY;
-        this.maxY = maxY;
-        this.accuracy = accuracy;
+        run(instance);
+    }
+
+    public static void run(Applet applet)
+    {
+        AccessController.doPrivileged(new PrivilegedAction<Object>()
+        {
+            public Object run()
+            {
+                frame.getContentPane().add(instance);
+                frame.setSize(500, 650);
+                applet.init();
+                applet.start();
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame.revalidate();
+                frame.repaint();
+                frame.setVisible(true);
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public void init()
+    {
+        AccessController.doPrivileged(new PrivilegedAction<Object>()
+        {
+            public Object run()
+            {
+                frame.getContentPane().add(instance);
+                frame.setSize(500, 650);
+
+                //min and max X fields
+                JPanel horizontalX = new JPanel();
+                horizontalX.setLayout(new BoxLayout(horizontalX, BoxLayout.LINE_AXIS));
+                horizontalX.add(minXLabel);
+                horizontalX.add(Box.createRigidArea(new Dimension(10, 0)));
+                horizontalX.add(minXText);
+                horizontalX.add(Box.createHorizontalGlue());
+                horizontalX.add(maxXLabel);
+                horizontalX.add(Box.createRigidArea(new Dimension(10, 0)));
+                horizontalX.add(maxXText);
+
+                //min and max Y fields
+                JPanel horizontalY = new JPanel();
+                horizontalY.setLayout(new BoxLayout(horizontalY, BoxLayout.LINE_AXIS));
+                horizontalY.add(minYLabel);
+                horizontalY.add(Box.createRigidArea(new Dimension(10, 0)));
+                horizontalY.add(minYText);
+                horizontalY.add(Box.createHorizontalGlue());
+                horizontalY.add(maxYLabel);
+                horizontalY.add(Box.createRigidArea(new Dimension(10, 0)));
+                horizontalY.add(maxYText);
+
+                //fucntion labels
+                JPanel horizontalLabels = new JPanel();
+                horizontalLabels.setLayout(new BoxLayout(horizontalLabels, BoxLayout.LINE_AXIS));
+                horizontalLabels.add(leftLabel);
+                horizontalLabels.add(Box.createHorizontalGlue());
+                horizontalLabels.add(topEqualsLabel);
+                horizontalLabels.add(Box.createHorizontalGlue());
+                horizontalLabels.add(rightLabel);
+
+                //left and right function fields
+                JPanel horizontalFuncs = new JPanel();
+                horizontalFuncs.setLayout(new BoxLayout(horizontalFuncs, BoxLayout.LINE_AXIS));
+                horizontalFuncs.add(Box.createHorizontalGlue());
+                horizontalFuncs.add(leftText);
+                horizontalFuncs.add(Box.createRigidArea(new Dimension(10, 0)));
+                horizontalFuncs.add(bottomEqualsLabel);
+                horizontalFuncs.add(Box.createRigidArea(new Dimension(10, 0)));
+                horizontalFuncs.add(rightText);
+                horizontalFuncs.add(Box.createHorizontalGlue());
+
+                //graph container
+                JPanel graph = new JPanel();
+                graph.setLayout(new BoxLayout(graph, BoxLayout.LINE_AXIS));
+                graph.add(graphedImage);
+
+                //vertical column displays everything
+                JPanel verticalColumn = new JPanel();
+                verticalColumn.setLayout(new BoxLayout(verticalColumn, BoxLayout.PAGE_AXIS));
+                verticalColumn.add(horizontalX);
+                verticalColumn.add(Box.createRigidArea(new Dimension(0, 5)));
+                verticalColumn.add(horizontalY);
+                verticalColumn.add(Box.createRigidArea(new Dimension(0, 10)));
+                verticalColumn.add(horizontalLabels);
+                verticalColumn.add(Box.createRigidArea(new Dimension(0, 5)));
+                verticalColumn.add(horizontalFuncs);
+                verticalColumn.add(Box.createRigidArea(new Dimension(0, 10)));
+                verticalColumn.add(graphButton);
+                verticalColumn.add(Box.createVerticalGlue());
+                verticalColumn.add(graph);
+
+                add(verticalColumn);
+
+                graphButton.addActionListener(e -> 
+                {
+                    graph.remove(graphedImage);
+                    graph.add(graphingText);
+
+                    //grab variables from text fields
+                    instance.l = leftText.getText();
+                    instance.r = rightText.getText();
+                    ActionListener thisAction = graphButton.getActionListeners()[0];
+                    if (thisAction != null)
+                    {
+                        //disable button while graphing
+                        graphButton.removeActionListener(thisAction);
+                    }
+                    try
+                    {
+                        //parse bounds
+                        instance.minX = ((Double)engine.eval(minXText.getText())).floatValue();
+                        instance.maxX = ((Double)engine.eval(maxXText.getText())).floatValue();
+                        instance.minY = ((Double)engine.eval(minYText.getText())).floatValue();
+                        instance.maxY = ((Double)engine.eval(maxYText.getText())).floatValue();
+
+                        //graph user input
+                        if (!graph())
+                        {
+                            //invalid input, read error. possible errors: l, r, x, y
+                        }
+                    }
+                    catch(ScriptException err)
+                    {
+                        //invalid input in the max/min fields
+                    }
+                    error = "";
+
+                    //re-enable button
+                    graphButton.addActionListener(thisAction);
+
+                    graph.remove(graphingText);
+                    graph.add(graphedImage);
+                });
+
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame.revalidate();
+                frame.repaint();
+                frame.setVisible(true);
+
+                return null;
+            }
+        });
+    }
+
+    public boolean graph()
+    {
         try
         {
-            float X, Y;
-            BufferedImage image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
-            for (X = 0; X < 500; X += 1.0 / accuracy)
+            if (maxX <= minX)
             {
-                for (Y = 0; Y < 500; Y += 1.0 / accuracy)
+                error = "x";
+                return false;
+            }
+            if (maxY <= minY)
+            {
+                error = "y";
+                return false;
+            }
+            float X, Y;
+            System.out.println("GRAPHING: " + l + " = " + r);
+            BufferedImage image = new BufferedImage((int)width, (int)height, BufferedImage.TYPE_INT_ARGB);
+            for (X = 0; X < width; X += 1.0 / accuracy)
+            {
+                if (((int)Math.floor(X * accuracy)) % 5 == 4)
                 {
-                    if (areEqual((X / (float)500.0) * (maxX - minX) + minX,(Y / (float)500.0) * (maxY - minY) + minY))
+                    System.out.print(".");
+                    if ((((int)Math.floor(X * accuracy)) / 5) % 4 == 0)
+                    {
+                        graphingText.setText("Graphing");
+                    }
+                    else
+                    {
+                        graphingText.setText(graphingText.getText() + ".");
+                    }
+                }
+                for (Y = 0; Y < height; Y += 1.0 / accuracy)
+                {
+                    if (areEqual((X / width) * (maxX - minX) + minX,(Y / height) * (maxY - minY) + minY))
                     {
                         image.setRGB((int)Math.floor(X), (int)Math.floor(Y), 0xFF000000);
                     }
                 }
             }
-            JFrame frame = new JFrame(l + " = " + r);
-            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            frame.add(new JLabel(new ImageIcon(image)));
-            frame.pack();
-            frame.setVisible(true);
+            graphedImage = new JLabel(new ImageIcon(image));
+            return true;
         }
         catch (ScriptException e)
         {
-            //invalid input
+            return false;
         }
     }
-    private static Boolean areEqual(float X, float Y) throws ScriptException
+    public Boolean areEqual(float X, float Y) throws ScriptException
     {
-        float upperLimit = L(X + ((maxX - minX) * tolerance), Y - ((maxY - minY) * tolerance));
-        float lowerLimit = L(X - ((maxX - minX) * tolerance), Y + ((maxY - minY) * tolerance));
+        float leftValue = -L(X, Y);
         float rightValue = R(X, Y);
+        float adjustedTolerance = (maxY - minY + maxX - minX) * tolerance / 2;
+        float upperLimit = leftValue - adjustedTolerance;
+        float lowerLimit = leftValue + adjustedTolerance;
         if (((lowerLimit <= rightValue) && (rightValue <= upperLimit)) || ((upperLimit <= rightValue) && (rightValue <= lowerLimit)))
         {
             return true;
@@ -74,23 +283,47 @@ public class Grapher extends Applet
     /*The following two functions should be edited to accomodate the pre-programmed relationship you wish to view. Order doesn't matter. For
     example, to view the relationship X * Y = X * sin(Y), enter X * Y into the return for the first function and X * Math.sin(Y)
     into the return statement of the second function*/
-    private static float L(float X, float Y) throws ScriptException
+    public float L(float X, float Y) throws ScriptException
     {
         Bindings binds = engine.getBindings(ScriptContext.ENGINE_SCOPE);
         binds.put("X", X);
         binds.put("Y", Y);
         binds.put("x", X);
         binds.put("y", Y);
-        return ((Double)engine.eval(l, binds)).floatValue();
+        try
+        {
+            return (float)engine.eval(l, binds);
+        }
+        catch (ScriptException e)
+        {
+            error = "l";
+            throw e;
+        }
+        catch (ClassCastException e)
+        {
+            return ((Double)engine.eval(l, binds)).floatValue();
+        }
     }
-    private static float R(float X, float Y) throws ScriptException
+    public float R(float X, float Y) throws ScriptException
     {
         Bindings binds = engine.getBindings(ScriptContext.ENGINE_SCOPE);
         binds.put("X", X);
         binds.put("Y", Y);
         binds.put("x", X);
         binds.put("y", Y);
-        return ((Double)engine.eval(r, binds)).floatValue();
+        try
+        {
+            return (float)engine.eval(r, binds);
+        }
+        catch (ScriptException e)
+        {
+            error = "r";
+            throw e;
+        }
+        catch (ClassCastException e)
+        {
+            return ((Double)engine.eval(r, binds)).floatValue();
+        }
     }
     /*My favorite relationships:
     Math.cos(X * X + Y * Y) = Math.sin(Math.sqrt(Math.abs(X * Y))) [-50,50]
@@ -101,19 +334,4 @@ public class Grapher extends Applet
     X / Math.cos(X - 2 * Y / X) = Y / Math.sin(2 * X + Y * Y / X)
     -X * Math.sin(X) = Y * Math.sin(Y)
     Y * X * Math.sin(Y) = Y * Math.cos(X * Y)*/
-
-
-    
-    // public String sayHello()
-    // {
-    //     return "Hello!";
-    // }
-
-    // private Class runner implements PrivilegedAction
-    // {
-    //     public Object run()
-    //     {
-    //         return this;
-    //     }
-    // }
 }
--- script.js
+++ script.js
@@ -1,55 +1,36 @@
 
-var altNav = function() {
-    document.getElementById("disappear").innerHTML = "<a href=games.html>Games</a>  |  <a href=art.html>Art</a>  |  <a href=poetry.html>Poetry</a>  |  <a href=other.html>Other</a>";
+var onWindowLoad = function(title) {
+    insertHUD(title);
+    windowScroll(null);
 }
 
-var regNav = function() {
-    document.getElementById("disappear").innerHTML = "<a href=projects.html>Projects</a>";
-}
 
-window.onscroll = scroll;
-function scroll(ev) {
-    if (window.pageYOffset < 79) {
-        document.getElementById("top").style.top = (-window.pageYOffset) + "px";
-        document.getElementById("pic").style.top = (50 - window.pageYOffset) + "px";
-        document.getElementById("nav").style.top = (75 - window.pageYOffset) + "px";
-        document.getElementById("main").style.top = (500 - window.pageYOffset) + "px";
+window.onscroll = windowScroll;
+function windowScroll(ev) {
+    if (document.getElementById("top")) {
+        //adjust page div offsets
+        if (window.pageYOffset < 79) {
+            document.getElementById("top").style.top = (-window.pageYOffset) + "px";
+            document.getElementById("pic").style.top = (50 - window.pageYOffset) + "px";
+            document.getElementById("nav").style.top = (75 - window.pageYOffset) + "px";
+            document.getElementById("main").style.top = (500 - window.pageYOffset) + "px";
+        } else if (window.pageYOffset >= 79) {
+            document.getElementById("top").style.top = "-79px";
+            document.getElementById("pic").style.top = "-29px";
+            document.getElementById("nav").style.top = "-4px";
+            document.getElementById("main").style.top = "421px";
+        }
     }
-    else if (window.pageYOffset >= 79) {
-        document.getElementById("top").style.top = "-79px";
-        document.getElementById("pic").style.top = "-29px";
-        document.getElementById("nav").style.top = "-4px";
-        document.getElementById("main").style.top = "421px";
-    }
 }
 
-var bgTopColors = ["#CCCCFF", "#CCFFCC", "#FFCCCC", "#FFFFCC"];
-var imgSrcs = ["images/blueMain.png", "images/greenMain.png", "images/redMain.png", "images/yellowMain.png"];
-var offsets = ["-200px", "-100px", "-350px", "0px"];
-var bgColors =["#888899", "#889988", "#998888", "#999988"];
-var curBG = 0;
-var changeColor = function() {
-    curBG++;
-    curBG %= bgTopColors.length;
-    setColorFields();
-    saveCookie();
-}
-var setColorFields = function() {
-    document.getElementById("top").style.backgroundColor = bgTopColors[curBG];
-    document.getElementById("image").src = "";
-    document.getElementById("crop").style.marginTop = offsets[curBG];
-    document.getElementById("image").src = imgSrcs[curBG];
-    document.getElementById("body").style.backgroundColor = bgColors[curBG];
-}
-var saveCookie = function() {
-    document.cookie = "savedBG=" + curBG;
-}
 
 var insertHUD = function(title) {
+    //initial cookie save if necessary
     if (document.cookie === "") {
         saveCookie();
     }
     curBG = document.cookie.substring(8, 9);
+    //just to cut down some repeated code in the html
     document.getElementById("hud").innerHTML =
         "<div id=\"top\">"+
             "<h1>" + title + "</h1>"+
@@ -79,7 +60,7 @@ var insertHUD = function(title) {
                         "<h3>|</h3>"+
                     "</td>"+
                     "<td class=\"rtd\">"+
-                        "<h3><a onclick=\"changeColor()\">Change Theme</a></h3>"+
+                        "<h3><a onclick=\"changeColor()\">Change Color</a></h3>"+
                     "</td>"+
                     "<td class=\"rtd\" style=\"width:5px\">"+
                         "<h3>|</h3>"+
@@ -95,13 +76,42 @@ var insertHUD = function(title) {
     setColorFields();
 }
 
+
+var bgTopColors = ["#CCCCFF", "#CCFFCC", "#FFCCCC", "#FFFFCC"];
+var imgSrcs = ["images/blueMain.png", "images/greenMain.png", "images/redMain.png", "images/yellowMain.png"];
+var offsets = ["-200px", "-100px", "-350px", "0px"];
+var bgColors = ["#888899", "#889988", "#998888", "#999988"];
+var curBG = 0;
+var changeColor = function() {
+    curBG++;
+    curBG %= bgTopColors.length;
+    setColorFields();
+    saveCookie();
+}
+var setColorFields = function() {
+    //change colored parts of the webpage
+    document.getElementById("top").style.backgroundColor = bgTopColors[curBG];
+    document.getElementById("image").src = "";
+    document.getElementById("crop").style.marginTop = offsets[curBG];
+    document.getElementById("image").src = imgSrcs[curBG];
+    document.getElementById("body").style.backgroundColor = bgColors[curBG];
+}
+
+
+var saveCookie = function() {
+    //save current background color
+    document.cookie = "savedBG=" + curBG;
+}
+
+
 var curIMG = 0;
 var cycleImages = function(num, amt) {
     curIMG++;
     curIMG %= amt;
-    var image = "images/art/artExample" + num + "" + curIMG + ".png";
+    var image = "images/art/artExample" + num + curIMG + ".png";
     document.getElementById("cycle" + num).src = image;
 }
+
 
 var fileNames = ["Intelligence Should Always Be Capitalized",
                  "Nighttime Walk",
@@ -121,4 +131,12 @@ var cyclePoetry = function() {
     curPoem++;
     curPoem %= fileNames.length;
     document.getElementById("poemTag").innerHTML = "<object data=\"poetry/" + fileNames[curPoem] + ".pdf\" type=\"application/pdf\" width=\"100%\" height=\"1075px\"></object>";
+}
+
+
+var altNav = function() {
+    document.getElementById("disappear").innerHTML = "<a href=games.html>Games</a>  |  <a href=art.html>Art</a>  |  <a href=poetry.html>Poetry</a>  |  <a href=other.html>Other</a>";
+}
+var regNav = function() {
+    document.getElementById("disappear").innerHTML = "<a href=projects.html>Projects</a>";
 }
